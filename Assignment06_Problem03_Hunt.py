c605e884af162916f70e799cf8b71fc42ba6697e
Alexander Hunt <hunt.al@northeastern.edu>
2024-04-08 01:12:01 -0400

    Added error handling.

--- Assignment06_Problem03_Hunt.py
+++ Assignment06_Problem03_Hunt.py
@@ -1,12 +1,21 @@
 class Employee():
     def __init__(self, name, position):
+        if not isinstance (name, str):
+            raise TypeError ("Variable type of name must be string!")
+        if not isinstance (position, str):
+            raise TypeError ("Variable type of position must be string!")
         self.name = name
         self.position = position
+        
     def calculate_salary(self):
         pass
     
 class HourlyEmployee(Employee):
     def __init__(self, name, position, hoursWorked, hourlyRate):
+        if not isinstance (hoursWorked, (int, float)):
+            raise TypeError ("Variable type of hours worked must be integer or float!")
+        if not isinstance (hourlyRate, (int, float)):
+            raise TypeError ("Variable type of hourly rate must be integer or float!")
         super().__init__(name, position)
         self.hoursWorked = hoursWorked
         self.hourlyRate = hourlyRate
@@ -16,6 +25,8 @@ class HourlyEmployee(Employee):
     
 class SalariedEmployee(Employee):
     def __init__(self, name, position, annualSalary):
+        if not isinstance (annualSalary, (int, float)):
+            raise TypeError ("Variable type of annual salary must be integer or float!")
         super().__init__(name, position)
         self.annualSalary = annualSalary
     
@@ -24,6 +35,10 @@ class SalariedEmployee(Employee):
 
 class CommissionEmployee(Employee):
     def __init__(self, name, position, comissionRate, target):
+        if not isinstance (comissionRate, (int, float)):
+            raise TypeError ("Variable type of commission rate must be integer or float!")
+        if not isinstance (target, int):
+            raise TypeError ("Variable type of commission target must be integer or float!")
         super().__init__(name, position)
         self.comissionRate = comissionRate
         self.target = target
@@ -44,5 +59,4 @@ employee3 = CommissionEmployee("Jane Doe", "Computer Scientist", 100, 100)
 list = [employee1, employee2, employee3]
 
 totalSalary(list)
-
     
